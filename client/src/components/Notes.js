2c798cb7f69b7397d868723d5de2ac1dc626f770
kanishk <kanishkshakya1@gmail.com>
2023-07-13 00:24:31 +0530

    Functionality Added Delete and Addnote WorkingDone

--- client/src/components/Notes.js
+++ client/src/components/Notes.js
@@ -1,17 +1,51 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect,useRef } from 'react'
 import noteContext from '../context/notes/noteContext'
 import NoteItem from './NoteItem'
 
 const Notes = () => {
     const context = useContext(noteContext)
-    const { notes } = context;
+    const { notes, getNotes } = context;
+    const updateNote = () => {
+        ref.current.click()
+    }
+    const ref = useRef(null)
+    useEffect(() => {
+        getNotes()
+        // eslint-disable-next-line
+    }, [])
+
     return (
         <>
+            {/* <!-- Button trigger modal --> */}
+            <button ref={ref} type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                Launch demo modal
+            </button>
+
+            {/* <!-- Modal --> */}
+            <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            ...
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="row my-3 mx-2">
                 <h1>Your Note</h1>
                 {notes.map((note) => {
                     console.log(note);
-                    return <NoteItem note={note} key={note._id} />
+                    return <NoteItem note={note} updateNote={updateNote} key={note._id} />
                 })}
             </div>
         </>
--- client/src/context/notes/NoteState.js
+++ client/src/context/notes/NoteState.js
@@ -1,48 +1,57 @@
 import NoteContext from "./noteContext";   // Warehouse  // inside the warehouse create context available
-import { useState } from "react"; 
+import { useState } from "react";
 const NoteState = (props) => {
   const host = 'https://black-hairdresser-mvcqo.pwskills.app:8000'
   const notesInitial = [
-    
-      {
-        "_id": "64abb3e6843a92f7865f1c7f",
-        "user": "64a9191fa7930a685da0a4a4",
-        "title": "Updating title",
-        "description": "Adding a description using a new Update",
-        "tag": "UpdatePurpose",
-        "date": "2023-07-10T07:31:50.901Z",
-        "__v": 0
-      },
-      {
-        "_id": "64abb460843a92f7865f1c83",
-        "user": "64a9191fa7930a685da0a4a4",
-        "title": "Adding a note using new Project file ",
-        "description": "I am adding this note for testing  purpose",
-        "tag": "General",
-        "date": "2023-07-10T07:33:52.406Z",
-        "__v": 0
-      }
-    ]
-  
+
+    {
+      "_id": "64abb3e6843a92f7865f1c7f",
+      "user": "64a9191fa7930a685da0a4a4",
+      "title": "Updating title",
+      "description": "Adding a description using a new Update",
+      "tag": "UpdatePurpose",
+      "date": "2023-07-10T07:31:50.901Z",
+      "__v": 0
+    }
+  ]
+
   const [notes, setNotes] = useState(notesInitial)
+
+  // --Get-All-Notes-------------------------------------------------//
+
+  const getNotes = async () => {
+
+    // API Call 
+    const response = await fetch(`${host}/api/notes/fetchallnotes`, {   //64abb3e6843a92f7865f1c7f
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRhOTE5MWZhNzkzMGE2ODVkYTBhNGE0In0sImlhdCI6MTY4ODk3Mzk2M30.t-8t7ckmHThLZLN4oJwvftTanhwg7qjhVLx86EiEaUM'
+      },
+    });
+    const json = await response.json();
+    console.log(json)
+    setNotes(json)
+  }
+
 
   // --Add-a-Note----------------------------------------------------//
 
 
 
-  const addNote = async (title,description,tag) => {
-    // Todo Api Call
+  const addNote = async (title, description, tag) => {
+
     // API Call 
-    const response = await fetch(`${host}/api/notes/addnote` , {   //64abb3e6843a92f7865f1c7f
+    const response = await fetch(`${host}/api/notes/addnote`, {   //64abb3e6843a92f7865f1c7f
       method: 'POST',
       headers: {
-        'Content-Type' : 'application/json',
-        'auth-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRhOTE5MWZhNzkzMGE2ODVkYTBhNGE0In0sImlhdCI6MTY4ODk3Mzk2M30.t-8t7ckmHThLZLN4oJwvftTanhwg7qjhVLx86EiEaUM'
+        'Content-Type': 'application/json',
+        'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRhOTE5MWZhNzkzMGE2ODVkYTBhNGE0In0sImlhdCI6MTY4ODk3Mzk2M30.t-8t7ckmHThLZLN4oJwvftTanhwg7qjhVLx86EiEaUM'
       },
-      body: JSON.stringify({title,description,tag})
+      body: JSON.stringify({ title, description, tag })
     });
     // const json =  response.json();
-     const note =  {
+    const note = {
       "_id": "64abb460843a9jhg2f78898965f1c83",
       "user": "64a9191fa7930a685da0a4a4",
       "title": title,
@@ -58,43 +67,55 @@ const NoteState = (props) => {
 
 
 
-  const deleteNote = (id) => {
+  const deleteNote = async (id) => {
+
+    // Api call
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {   //64abb3e6843a92f7865f1c7f
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRhOTE5MWZhNzkzMGE2ODVkYTBhNGE0In0sImlhdCI6MTY4ODk3Mzk2M30.t-8t7ckmHThLZLN4oJwvftTanhwg7qjhVLx86EiEaUM'
+      },
+    });
+    const json = response.json();
+    console.log(json)
     console.log("Deleteing the note with id :" + id)
-    const newNotes = notes.filter((note) => {return note._id!==id})
+    const newNotes = notes.filter((note) => { return note._id !== id })
     setNotes(newNotes)
+
   }
 
   // --Edit-a-Note-----------------------------------------------------// 
 
 
 
-  const editNote = async (id,title,description,tag)=> {
+  const editNote = async (id, title, description, tag) => {
     // Api call
-    const response = await fetch(`${host}/api/notes/updatenote/${id}` , {   //64abb3e6843a92f7865f1c7f
+    const response = await fetch(`${host}/api/notes/updatenote/${id}`, {   //64abb3e6843a92f7865f1c7f
       method: 'PUT',
       headers: {
-        'Content-Type' : 'application/json',
-        'auth-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRhOTE5MWZhNzkzMGE2ODVkYTBhNGE0In0sImlhdCI6MTY4ODk3Mzk2M30.t-8t7ckmHThLZLN4oJwvftTanhwg7qjhVLx86EiEaUM'
+        'Content-Type': 'application/json',
+        'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRhOTE5MWZhNzkzMGE2ODVkYTBhNGE0In0sImlhdCI6MTY4ODk3Mzk2M30.t-8t7ckmHThLZLN4oJwvftTanhwg7qjhVLx86EiEaUM'
       },
-      body: JSON.stringify({title,description,tag})
+      body: JSON.stringify({ title, description, tag })
     });
-    const json =  response.json();
+    const json = response.json();
     console.log(json)
 
     // Logic to edit in client 
     for (let index = 0; index < notes.length; index++) {
       const element = notes[index];
 
-      if(element._id === id){
+      if (element._id === id) {
         element.title = title;
         element.description = description;
         element.tag = tag;
-      } 
-      
+      }
+
     }
   }
   return (
-    <NoteContext.Provider value={{ notes, addNote,deleteNote,editNote }}>
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote, getNotes }}>
       {props.children}
     </NoteContext.Provider>
   )
